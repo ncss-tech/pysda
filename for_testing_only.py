24afd1aa085e9e003942690810e26aa6e6aab64e
Ferguson <12001000113634@FEDIDCARD.GOV>
2021-05-21 21:04:14 -0500

    folium
    folium example

--- for_testing_only.py
+++ for_testing_only.py
@@ -13,9 +13,11 @@ import sdapoly, sdaprop, sdainterp
 
 
 myshp = r"F:\ZBOOK\GIS\TEMP\fs.shp"
-
 myaoi = sdapoly.shp(myshp, export = True, name = 'fto2.shp')
-wtdavg = sdaprop.getprop(myaoi, 'mukey', top = 0, bottom = 100, prop = 'sandtotal_r', prnt=False)
+
+test_aoi = sdapoly.gdf(myaoi)
+
+wtdavg = sdaprop.getprop(df = myaoi, column ='mukey', method = 'wtd_avg', top = 0, bottom = 100, prop = 'sandtotal_r', minmax = None, prnt=False, meta = False)
 domcompnum = sdaprop.getprop(myaoi, 'mukey', 'claytotal_r', method = 'dom_comp_num', top = 300, bottom = 400, prnt = False)
 minmax = sdaprop.getprop(myaoi, 'mukey', 'claytotal_r', method = 'minmax', top = 20, bottom = 55, minmax = 'MAX', prnt = False)
 domcond = sdaprop.getprop(myaoi, 'mukey', 'taxorder', method = 'dom_cond', prnt = False)
@@ -25,3 +27,5 @@ intdomcomp = sdainterp.getinterp(myaoi, 'mukey', 'WMS - Pond Reservoir Area', 'd
 intwta = sdainterp.getinterp(myaoi, 'mukey', 'Ground Penetrating Radar Penetration', 'wtd_avg')
 intdomcond = sdainterp.getinterp(myaoi, 'mukey', 'ENG - Shallow Excavations', method = 'dom_cond', prnt = True)
 
+import pandas as pd, geopandas as gpd
+myaoi = myaoi.merge(wtdavg, how='inner', on='mukey')
