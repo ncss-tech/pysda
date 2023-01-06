e73b11ba0f060ba63eb760499b76670665e5c475
cferguso <charles.ferguson@lin.usda.gov>
2023-01-06 14:11:43 -0600

    added all

--- __init__.py
+++ __init__.py
@@ -5,3 +5,11 @@ Created on Fri Mar  4 15:04:19 2022
 @author: Charles.Ferguson
 """
 
+__all__ = [
+        'sdainterp',
+        'sdapoly',
+        'sdaprop',
+        'sdatab',
+        'sdawss'
+        ]
+        
